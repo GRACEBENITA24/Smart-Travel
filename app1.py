e27c8f4063dfc0a20bf8218dc75f2f00f7f3cfc5
GRACEBENITA24 <119575739+GRACEBENITA24@users.noreply.github.com>
2025-09-24 13:21:05 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -93,7 +93,7 @@ inject_css()
 
 # ----------------- PAGES -----------------
 if menu_choice == "🏠 Home":
-    set_background("sky.jpg")
+    set_background("background.jpg")
     st.markdown(
         """
         <div class="overlay-container" style="text-align:center; padding-top:15%;">
